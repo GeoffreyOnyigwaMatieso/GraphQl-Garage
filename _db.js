cd45a272f656b8c32346cba5b6dcddbcab145a85
Tech_G <matiesogeoffrey@gmail.com>
2024-06-17 14:57:29 +0300

    fix: resolved data return null

--- _db.js
+++ _db.js
@@ -1,26 +1,24 @@
 let games = [
-    {id: 1, title: "Super Mario Bros", platform: ["NES"]},
-    {id: 2, title: "Super Mario Bros 2", platform: ["NES", "XBOX"]},
-    {id: 3, title: "Super Mario Bros 3", platform: ["NES","PS5", "PC"]},
-    {id: 4, title: "Super Mario World", platform: ["SNES"]},
-    {id: 5, title: "Super Mario 64", platform: ["N64","DS","Wii","WiiU","Switch"]},
-]
-
-let authors = [ 
-    {id: 1, name: "Mario", verified: true},
-    {id: 2, name: "Yoshi", verified: false},
-    {id: 3, name: "Peach", verified: true},
-
-]
-
-let reviews = [
-    {id: 1, rating: 5, content: "Great game", author_id: 1, game_id: 2},
-    {id: 2, rating: 4, content: "Good game", author_id: 2, game_id: 3},
-    {id: 3, rating: 3, content: "Ok game", author_id: 3, game_id: 1},
-    {id: 4, rating: 2, content: "Bad game", author_id: 1, game_id: 4},
-    {id: 5, rating: 1, content: "Terrible game", author_id: 2, game_id: 5},
-    {id: 6, rating: 5, content: "Great game", author_id: 3, game_id: 1},
-    {id: 7, rating: 4, content: "Good game", author_id: 1, game_id: 2},
-]
-
+    {id: '1', title: 'Zelda, Tears of the Kingdom', platform: ['Switch']},
+    {id: '2', title: 'Final Fantasy 7 Remake', platform: ['PS5', 'Xbox']},
+    {id: '3', title: 'Elden Ring', platform: ['PS5', 'Xbox', 'PC']},
+    {id: '4', title: 'Mario Kart', platform: ['Switch']},
+    {id: '5', title: 'Pokemon Scarlet', platform: ['PS5', 'Xbox', 'PC']},
+  ]
+  
+  let authors = [
+    {id: '1', name: 'mario', verified: true},
+    {id: '2', name: 'yoshi', verified: false},
+    {id: '3', name: 'peach', verified: true},
+  ]
+  
+  let reviews = [
+    {id: '1', rating: 9, content: 'kilometerfuyo', author_id: '1', game_id: '2'},
+    {id: '2', rating: 10, content: 'soweto', author_id: '2', game_id: '1'},
+    {id: '3', rating: 7, content: 'aleluyaaleluya', author_id: '3', game_id: '3'},
+    {id: '4', rating: 5, content: 'balanciego', author_id: '2', game_id: '4'},
+    {id: '5', rating: 8, content: 'kirikatasoweto', author_id: '2', game_id: '5'},
+    {id: '6', rating: 7, content: 'skibidii', author_id: '1', game_id: '2'},
+    {id: '7', rating: 10, content: 'skiboo', author_id: '3', game_id: '1'},
+  ]
 export default {games, authors, reviews }
